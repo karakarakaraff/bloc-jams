6c1d95c5d434d857fc101d2df97338e52bcbc399
Kara <kara.a.flaherty@gmail.com>
2017-03-01 01:15:49 -0700

    Assignment 8 DOM scripting: animation: updated

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,15 @@
-var revealPoint = function() {
-
+var animatePoints = function() {
     var points = document.getElementsByClassName('point');
     
-    for (var i = 0; i <= points.length; i++) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    var revealPoint = function(index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     };
+    
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(i);
+    }
 
 };
