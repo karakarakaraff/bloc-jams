7ef5e693b2ae5e1b296a8d14ad94e1f7f11f6ea5
Kara <kara.a.flaherty@gmail.com>
2017-03-04 12:20:06 -0700

    Assignment 9 DOM Scripting: Events: updated

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,19 +1,17 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(point) {
+var animatePoints = function(points) {
     
-    var revealPoint = function(point) {
-        point.style.opacity = 1;
-        point.style.transform = "scaleX(1) translateY(0)";
-        point.style.msTransform = "scaleX(1) translateY(0)";
-        point.style.WebkitTransform = "scaleX(1) translateY(0)";
+    var revealPoint = function(index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     };
     
-    forEach(pointsArray, revealPoint);
-    
-    //for (var i = 0; i < points.length; i++) {
-    //    revealPoint(i);
-    //}
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(i);
+    }
 
 };
 
