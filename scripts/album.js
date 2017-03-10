47ccbb025300f597229d741c2a46041558c8eab8
Kara <kara.a.flaherty@gmail.com>
2017-03-10 12:10:07 -0700

    Assignment 13 DOM Scripting: Play/Pause part 2: attempt 1

--- scripts/album.js
+++ scripts/album.js
@@ -62,7 +62,11 @@ var setCurrentAlbum = function(album) {
 var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
-        while (currentParent.className !== targetClass && currentParent.className !== null) {
+        if (currentParent === null) {
+            console.log('No parent found');
+        } else if (currentParent.className === null) {
+            console.log('No parent found with that class name');
+        } else if (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
         return currentParent;
