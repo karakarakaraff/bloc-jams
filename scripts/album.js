fa2aad905b895e66ceafc5023f5e5ecb4455a5a5
Kara <kara.a.flaherty@gmail.com>
2017-03-16 01:22:26 -0600

    Assignment 13 DOM Scripting: Play/Pause 2: update 1

--- scripts/album.js
+++ scripts/album.js
@@ -62,12 +62,13 @@ var setCurrentAlbum = function(album) {
 var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
+        while (currentParent.className !== targetClass && currentParent.className !== null) {
+            currentParent = currentParent.parentElement;
+        }
         if (currentParent === null) {
             console.log('No parent found');
         } else if (currentParent.className === null) {
             console.log('No parent found with that class name');
-        } else if (currentParent.className !== targetClass && currentParent.className !== null) {
-            currentParent = currentParent.parentElement;
         }
         return currentParent;
     }
